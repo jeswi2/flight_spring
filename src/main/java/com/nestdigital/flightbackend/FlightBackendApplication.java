ed71b68886600361a221f6c8d155630dcc7abc7b
jeswi2 <jeswimathewm@gmail.com>
2022-10-19 15:23:22 +0530

    ma

---
+++ src/main/java/com/nestdigital/flightbackend/FlightBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.flightbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/flightbackend/FlightBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.flightbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
